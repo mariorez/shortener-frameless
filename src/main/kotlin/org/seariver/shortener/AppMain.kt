c4e0a3db433fba175eb9e5b15c6d9e895c5c50cf
Mario Rezende <mariorez@gmail.com>
2021-08-10 19:12:40 -0300

    feat: implement DiC successful

--- src/main/kotlin/org/seariver/shortener/AppMain.kt
+++ src/main/kotlin/org/seariver/shortener/AppMain.kt
@@ -1,10 +1,32 @@
 package org.seariver.shortener
 
+import org.flywaydb.core.Flyway
+import org.seariver.shortener.lib.DiC
+import java.io.File
+import java.util.*
+
 fun main() {
 
-    GrpcServer(50051).run {
-        this.start()
-        this.blockUntilShutdown()
-    }
+    val input = File(
+        "/home/mario/Development/shortener-frameless/src/main/resources/application.properties"
+    ).inputStream()
 
+    val properties = Properties()
+    properties.load(input)
+
+    System.setProperty("jdbc.url", properties.getProperty("jdbc.url"))
+    System.setProperty("jdbc.username", properties.getProperty("jdbc.username"))
+    System.setProperty("jdbc.password", properties.getProperty("jdbc.password"))
+    System.setProperty("jdbc.driverClassName", properties.getProperty("jdbc.driverClassName"))
+
+    val dic = DiC()
+    val dataSource = dic.getDataSource()
+
+    val flyway = Flyway.configure().dataSource(dataSource).load()
+    flyway.migrate()
+
+    GrpcServer(50051, dic).run {
+        start()
+        blockUntilShutdown()
+    }
 }
