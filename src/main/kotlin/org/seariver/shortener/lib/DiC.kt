46ed0e175b4ffa7064ab4d8383c6e6055c7639c4
Mario Rezende <mariorez@gmail.com>
2021-08-14 10:15:01 -0300

    perf: refactor DiC as Singleton

--- src/main/kotlin/org/seariver/shortener/lib/DiC.kt
+++ src/main/kotlin/org/seariver/shortener/lib/DiC.kt
@@ -2,35 +2,52 @@ package org.seariver.shortener.lib
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import javax.sql.DataSource
 import org.seariver.shortener.adapter.`in`.ShortenerWriteService
 import org.seariver.shortener.adapter.out.ShortenerRepositoryImpl
 import org.seariver.shortener.application.port.out.ShortenerRepository
 import org.seariver.shortener.application.usecase.ShortenUrlHandler
-import javax.sql.DataSource
 
 class DiC {
 
+    private var dataSource: DataSource? = null
+    private var shortenerRepository: ShortenerRepository? = null
+    private var shortenerHandler: ShortenUrlHandler? = null
+    private var shortenerWriteService: ShortenerWriteService? = null
+
     fun getDataSource(): DataSource {
 
-        val hikariConfig = HikariConfig().apply {
-            jdbcUrl = System.getProperty("jdbc.url")
-            username = System.getProperty("jdbc.username")
-            password = System.getProperty("jdbc.password")
-            driverClassName = System.getProperty("jdbc.driverClassName")
-        }
+        return dataSource ?: run {
+            val hikariConfig = HikariConfig().apply {
+                jdbcUrl = System.getProperty("jdbc.url")
+                username = System.getProperty("jdbc.username")
+                password = System.getProperty("jdbc.password")
+                driverClassName = System.getProperty("jdbc.driverClassName")
+            }
 
-        return HikariDataSource(hikariConfig)
+            dataSource = HikariDataSource(hikariConfig)
+            dataSource as DataSource
+        }
     }
 
     fun getShortenerRepository(): ShortenerRepository {
-        return ShortenerRepositoryImpl(getDataSource())
+        return shortenerRepository ?: run {
+            shortenerRepository = ShortenerRepositoryImpl(getDataSource())
+            shortenerRepository as ShortenerRepository
+        }
     }
 
     fun getShortenUrlHandler(): ShortenUrlHandler {
-        return ShortenUrlHandler(getShortenerRepository())
+        return shortenerHandler ?: run {
+            shortenerHandler = ShortenUrlHandler(getShortenerRepository())
+            shortenerHandler as ShortenUrlHandler
+        }
     }
 
     fun getShortenerWriteService(): ShortenerWriteService {
-        return ShortenerWriteService(getShortenUrlHandler())
+        return shortenerWriteService ?: run {
+            shortenerWriteService = ShortenerWriteService(getShortenUrlHandler())
+            shortenerWriteService as ShortenerWriteService
+        }
     }
 }
