592b0f561419a678c91735c77620e9a351817aec
Mario Rezende <mariorez@gmail.com>
2021-08-15 19:20:13 -0300

    refactor: convert DiC to Object

--- src/main/kotlin/org/seariver/shortener/lib/DiC.kt
+++ src/main/kotlin/org/seariver/shortener/lib/DiC.kt
@@ -8,19 +8,17 @@ import org.seariver.shortener.adapter.out.ShortenerRepositoryImpl
 import org.seariver.shortener.application.port.out.ShortenerRepository
 import org.seariver.shortener.application.usecase.ShortenUrlHandler
 
-class DiC {
-    companion object {
-        val dataSource: DataSource by lazy {
-            val hikariConfig = HikariConfig().apply {
-                jdbcUrl = System.getProperty("jdbc.url")
-                username = System.getProperty("jdbc.username")
-                password = System.getProperty("jdbc.password")
-                driverClassName = System.getProperty("jdbc.driverClassName")
-            }
-            HikariDataSource(hikariConfig)
+object DiC {
+    val dataSource: DataSource by lazy {
+        val hikariConfig = HikariConfig().apply {
+            jdbcUrl = System.getProperty("jdbc.url")
+            username = System.getProperty("jdbc.username")
+            password = System.getProperty("jdbc.password")
+            driverClassName = System.getProperty("jdbc.driverClassName")
         }
-        val shortenerRepository: ShortenerRepository by lazy { ShortenerRepositoryImpl(dataSource) }
-        val shortenerHandler: ShortenUrlHandler by lazy { ShortenUrlHandler(shortenerRepository) }
-        val shortenerWriteService: ShortenerWriteService by lazy { ShortenerWriteService(shortenerHandler) }
+        HikariDataSource(hikariConfig)
     }
+    val shortenerRepository: ShortenerRepository by lazy { ShortenerRepositoryImpl(dataSource) }
+    val shortenerHandler: ShortenUrlHandler by lazy { ShortenUrlHandler(shortenerRepository) }
+    val shortenerWriteService: ShortenerWriteService by lazy { ShortenerWriteService(shortenerHandler) }
 }
